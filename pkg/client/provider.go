2de1142e3817f99cf4688469d9b6d3f257d318d9
nicobarra <nbarragan01@gmail.com>
2023-02-10 16:08:05 -0300

    feat(client): defined pkg with exported types for jobs and providers

---
+++ pkg/client/provider.go
@@ -0,0 +1,19 @@
+package client
+
+// Struct for DB
+type Provider struct {
+	ID       string   `json:"id"`
+	Name     string   `json:"name"`
+	Networks []string `json:"networks"`
+}
+
+// Define state variables for the provider's jobs
+type State string
+
+const (
+	StatusIdle        State = "idle"
+	StatusRunning     State = "running"
+	StatusStopped     State = "stopped"
+	StatusError       State = "error"
+	StatusAutoStopped State = "autoStopped"
+)
